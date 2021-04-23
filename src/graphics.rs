c3522864f6515f0ca8a8aac197382c95a7642c6f
kalwalt <github@kalwaltart.it>
2021-04-23 21:38:49 +0200

    using hollow_rects to better visualize

--- src/graphics.rs
+++ src/graphics.rs
@@ -1,6 +1,6 @@
 use image::{ Rgb, RgbImage };
 use imageproc::rect::Rect;
-use imageproc::drawing::draw_filled_rect_mut;
+use imageproc::drawing::draw_hollow_rect_mut;
 
 pub struct Graphics;
 
@@ -8,7 +8,7 @@ impl Graphics {
     pub fn draw_rects(canvas: &mut RgbImage,  num: i32) {
         let white = Rgb([255u8, 255u8, 255u8]);
         for a in 0..num {
-            draw_filled_rect_mut(canvas, Rect::at(60, 10 + a).of_size(250, 200), white);
+            draw_hollow_rect_mut(canvas, Rect::at(60, 10 + (a * 4)).of_size(250, 200), white);
         }
     }
 }
